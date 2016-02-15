1ccd6a32bff04a4452a1e26fd7724e6d35d345dd
Jan Noha <nohajc@gmail.com>
2016-02-15 21:42:19 +0100

    Added executable and a RTWindow class.

---
+++ lib/ruby_tower/window.rb
@@ -0,0 +1,16 @@
+require "gosu"
+
+module RubyTower
+	class RTWindow < Gosu::Window
+		def initialize
+			super 1024, 768
+			caption = "Ruby Tower"
+		end
+
+		def update
+		end
+
+		def draw
+		end
+	end
+end
